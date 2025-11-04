52af8402a81ce797356e5f20db2ad90549a9d8fe
Giovanni Caracillo <giovanni.caracillo@sherwin.com>
2025-11-04 12:22:20 -0500

    Remove client_secret from Azure provider configuration for security reasons

--- provider.tf
+++ provider.tf
@@ -17,6 +17,5 @@ provider "azurerm" {
   features {}
   tenant_id       = var.tenant_id
   client_id       = var.client_id
-  client_secret   = var.client_secret
   subscription_id = var.subscription_id
 }
